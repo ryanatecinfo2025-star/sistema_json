2fdd6117db4fdb5f5d6fd301397fe22f2a63e588
isadolivabreu-droid <isadolivabreu@gmail.com>
2026-09-17 16:16:41 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -14,7 +14,13 @@ while digitado !=6:
     digitado = input("escolha alguma opçâo")
 
     if digitado == "1":
-       print("esta pessoa ficará com cadastro")
+      nome = input("Digite o nome completo do paciente: ")
+      ''' print("Digite o CPF do paciente: ")
+       print("Digite a data de nascimento do paciente: ")
+       print("Digite o sexo do paciente: ")
+       print("Digite o endereço do paciente: ")
+       print("Digite o telefone do paciente: ")
+       print("Digite os sintomas do paciente: ")'''
 
     if digitado == "2":
          print("esta pessoa ficará com Exibir")
