fdbbcdb76b64a09d39365a4ccff92d852b1e2540
Jorta-ghost <ryanatec.info2025@gmail.com>
2026-09-24 15:46:03 -0300

    att

--- projeto.py
+++ projeto.py
@@ -14,6 +14,7 @@ while digitado != "0":
     digitado = input("Escolha alguma opção: ")
 
     if digitado == "1":
+<<<<<<< Updated upstream
         nome = input("Digite o nome completo do paciente: ")
         cpf = input("Digite o CPF do paciente: ")
         data_de_nascimento = input("Digite a data de nascimento do paciente: ")
@@ -39,3 +40,31 @@ while digitado != "0":
         inventário.append(paciente)
         print(f"\n Paciente {nome} cadastrado com sucesso!")
         
+=======
+       print("Digite o nome completo do paciente: ")
+       print("Digite o CPF do paciente: ")
+       print("Digite a data de nascimento do paciente: ")
+       print("Digite o sexo do paciente: ")
+       print("Digite o endereço do paciente: ")
+       print("Digite telefone para contato: ")
+       print("Digite os sintomas do paciente: ")
+
+    if digitado == "2":
+         print("esta pessoa ficará com Exibir")
+
+    if digitado == "3":
+       print("esta pessoa ficará com Editar")
+
+    if digitado == "4":
+       print("esta pessoa ficará com Deletar")
+
+    if digitado == "5":
+       print("esta pessoa ficará com Pesquisar")
+
+    if digitado == "6":
+       print("esta pessoa ficará com Gerar Relatório")
+
+    if digitado == "0":
+       break
+   
+>>>>>>> Stashed changes
